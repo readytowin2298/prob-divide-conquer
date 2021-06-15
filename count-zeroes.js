2a655314a8e781227508f83a1c9cbfdb6c88f91c
David Ames <readytowin2298@gmail.com>
2021-06-14 22:36:01 -0500

    finished first challenge

--- count-zeroes.js
+++ count-zeroes.js
@@ -4,28 +4,31 @@
 // returns the number of zeroes in the array.
 
 function countZeroes(arr) {
-  
+    // Find first 0 
+    let first = binaryIndex0(arr)
 
-  
+    // Return 0 if no 0 is found
+    if (first === -1) return 0
+
+    // Return num of 0's
+    return arr.length - first;
 }
 
-function binaryIndex0(arr){
-    let low = 0;
-    let high = arr.length -1;
-
-    while (low <= high) {
-        let midI = low + Math.floor((low + high) / 2);
-
-        if((midI === 0 || arr[midI - 1] === 1) && arr[midI] === 0){
-            return midI;
-        } else if (arr[midI] === 1){
-            low = midI + 1;
-        } else {
-            high = midI - 1
+function binaryIndex0(arr, low = 0, high = arr.length - 1) {
+    // Catch if no 0 is found
+    if (high >= low) {
+    
+        let mid = low + Math.floor((high - low) / 2)
+        if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+            return mid;
+        } else if (arr[mid] === 1) {
+            return binaryIndex0(arr, mid + 1, high)
         }
-    }
+        return binaryIndex0(arr, low, mid - 1)
+        }
     return -1;
-}
+  }
+  
 
 // module.exports = countZeroes
-module.exports = binaryIndex0;
+module.exports = countZeroes;
